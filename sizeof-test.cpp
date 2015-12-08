a4d872ab540d092c96047cbea2a605f1d60bf1d6
Boush Li <li.boush@gmail.com>
2015-12-08 14:50:49 +0800

    o Add test for sizeof.

---
+++ sizeof-test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        cout << "sizeof(int) = " << sizeof(int) << endl;
+        cout << "sizeof(long) = " << sizeof(long) << endl;
+        cout << "sizeof(float) = " << sizeof(float) << endl;
+        cout << "sizeof(double) = " << sizeof(double) << endl;
+        return 0;
+}
