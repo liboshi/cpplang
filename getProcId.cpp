249ba68e1c34162d49c0879259ffc94b24362429
Boush Li <li.boush@gmail.com>
2016-05-06 23:40:36 +0800

    o Add function to get process id by name.

---
+++ getProcId.cpp
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vecter.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int
+getProcIdByName(string procName)
+{
+}
