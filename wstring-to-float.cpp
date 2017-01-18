a48dda03032a00b75a3c7fb1df87817796dbd9dd
Boshi Li <boshil@vmware.com>
2017-01-18 15:25:09 +0800

    o Update.

--- wstring-to-float.cpp
+++ wstring-to-float.cpp
@@ -3,13 +3,19 @@
 #include <string>
 #include <cstring>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PATH 256
+
+using namespace std;
 
 int
 main() {
-    std::wstring ret = :"Hello wstring";
+    std::wstring ret = L"1024";
     wcout << ret << endl;
     char *str = new char[MAX_PATH];
     wcstombs(str, ret.c_str(), MAX_PATH);
     cout << "Hello: " << str << endl;
-    cout << ::atof(str) / 1024.00 << endl;
+    wcout << ::atof(str) / 1024.00 << endl;
 }
